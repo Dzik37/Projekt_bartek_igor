a82fa961c2ac1f001200607db02a42c10a482a92
Dzik37 <igor.edek@gmail.com>
2024-04-22 13:33:13 +0200

    dodanie trasformacji

--- projekt_Bartek_igor.py
+++ projekt_Bartek_igor.py
@@ -1,7 +1,22 @@
 import numpy as np
 from wzory_gw import *
-f = radians(52)
-l = radians(21)
-h=100
-x,y,z = flh2xyz(f, l, h, a, e2)
-print(x,y,z)
+
+class Transformacje:
+    def __init__(self, model: str = "wgs84"):
+        """
+        Parametry elipsoid:
+            a - duża półoś elipsoidy - promień równikowy
+            e2 - mimośród^2
+        """
+        if model == "wgs84":
+            self.a = 6378137.0 
+            self.e2 = 0.006694379990141124
+        elif model == "grs80":
+            self.a = 6378137.0
+            self.e2 = 0.00669438002290
+        elif model == "krasowski":
+            self.a = 6378245.000 
+            self.e2 = 0.00669342162296
+        else:
+            raise NotImplementedError(f"{model} model not implemented")
+       
